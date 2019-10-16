108113d7322e2928c6ead70b39520a3b734b7c77
Lu-Thao <thaol31@gmatc.matc.edu>
2019-10-16 17:47:57 -0500

    post table

--- Domain/Posts.cs
+++ Domain/Posts.cs
@@ -4,7 +4,7 @@ namespace Domain
     public class Post
     {
         public Guid Id { get; set; }
-        public string Titile { get; set; }
+        public string Title { get; set; }
         public string Body { get; set;}
         public DateTime Date { get; set;}
     }
