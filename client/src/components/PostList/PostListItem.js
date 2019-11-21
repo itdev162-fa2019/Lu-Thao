5a055dc3bd4e83bdadc4b6a6280fc2125ab7324a
Lu-Thao <thaol31@gmatc.matc.edu>
2019-11-21 17:08:49 -0600

    completed activity-9

--- client/src/components/PostList/PostListItem.js
+++ client/src/components/PostList/PostListItem.js
@@ -12,17 +12,19 @@ const PostListItem = props => {
 
         clickPost(post);
         history.push(`/posts/${slug}`);
-    }
+    };
 
     return (
-        <div className="postListItem" onClick={() => handleClickPost(post)}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+        <div>
+            <div className="postListItem" onClick={() => handleClickPost(post)}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            <div className="postControls">
+                <button onClick={() => deletePost(post)}>Delete</button>
+            </div>
         </div>
-        <div className="postControls">
-            <button onClick={() => deletePost(post)}>Delete</button>
-        </div>
-    )
-}
+    );
+};
 
 export default PostListItem;
